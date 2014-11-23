a263da29b72e1c47087e2e0b681d9dd9b7ff2b57
Daniel Büchele <daniel@buechele.cc>
2014-11-23 10:00:50 -0800

    -

--- site/tags/honeycomb.php
+++ site/tags/honeycomb.php
@@ -1,9 +1,10 @@
 <?php	
 	kirbytext::$tags['honeycomb'] = array(
 		'html' => function($tag) {
-			echo '<div class="honeycomb">';
-			echo '<img src="'.thumb($tag->page()->image($tag->attr('honeycomb')), array('width' => 200,'height' => 200, 'crop' => true))->url().'" />';
-			echo '</div>';
+			$r = '<div class="honeycomb">';
+			$r .= '<img src="'.thumb($tag->page()->image($tag->attr('honeycomb')), array('width' => 200,'height' => 200, 'crop' => true))->url().'" />';
+			$r .= '</div>';
+			return $r;
 		}
 	);
 ?>
