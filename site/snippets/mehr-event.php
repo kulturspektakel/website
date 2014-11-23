dea982286a927ebfe79bfbe648935ffdb2e3dd5b
Daniel Büchele <daniel@buechele.cc>
2014-11-22 22:15:43 -0800

    -

--- site/snippets/mehr-event.php
+++ site/snippets/mehr-event.php
@@ -1,20 +1,22 @@
 <div class="row more-event">
 
-	<div class="col-sm-6 col-photo <?php if ($i%2==1) {echo " col-sm-push-6 ";} ?>">
+	<div class="col-sm-4 col-photo <?php if ($i%2==1) {echo " col-sm-push-8 ";} ?>">
 		<div class="honeycomb">
 		<?php if ($p->hasImages()) echo '<img src="'.thumb($p->images()->first(),array('width' => 800, 'height' => '800', 'crop' => true))->url().'" />'; ?>
 		</div>
 	</div>
 	
-	<div class="col-sm-6 col-info <?php if ($i%2==1) {echo " col-sm-pull-6 ";} ?>">
-		<h2>
-			<?=$p->title() ?>
-		</h2>
-		<div class="date">
-			<?=$p->datum() ?>
-		</div>
-		<div class="text">
-			<?=kirbytext($p->text()) ?>
+	<div class="col-sm-8 col-info <?php if ($i%2==1) {echo " col-sm-pull-4 ";} ?>">
+		<div class="col-info-inner">
+			<h2>
+				<?=$p->title() ?>
+			</h2>
+			<div class="date">
+				<?=$p->datum() ?>
+			</div>
+			<div class="text">
+				<?=kirbytext($p->text()) ?>
+			</div>
 		</div>
 	</div>
 
