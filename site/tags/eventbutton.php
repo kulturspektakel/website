ecdafed47ff18f03ab96914b664e326646f5e636
Daniel Büchele <daniel@buechele.cc>
2017-02-20 21:27:37 +0100

    missing semicolon

--- site/tags/eventbutton.php
+++ site/tags/eventbutton.php
@@ -2,7 +2,7 @@
 	kirbytext::$tags['eventbutton'] = array(
 		'html' => function($tag) {
 			$r = '<a class="btn btn-danger btn-block" target="_blank" onclick="fbq(\'track\', \'CompleteRegistration\');" href="'.$tag->attr('link').'">';
-			$r .= $tag->attr('eventbutton')
+			$r .= $tag->attr('eventbutton');
 			$r .= '</a>';
 			return $r;
 		}
