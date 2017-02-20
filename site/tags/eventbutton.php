68c58644fc9477864df5b4a629fc1868f7c1d62f
Daniel Büchele <daniel@buechele.cc>
2017-02-20 21:39:16 +0100

    adding button attribute text

--- site/tags/eventbutton.php
+++ site/tags/eventbutton.php
@@ -1,5 +1,8 @@
 <?php
 	kirbytext::$tags['eventbutton'] = array(
+		'attr' => array(
+			'text'
+		),
 		'html' => function($tag) {
 			return '<a class="btn btn-danger btn-block" target="_blank" onclick="fbq(\'track\', \'CompleteRegistration\');" href="' . $tag->attr('eventbutton') . '">' . $tag->attr('text') . '</a>';
 		}
