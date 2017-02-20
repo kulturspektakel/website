d09d85b694735865b153c759f229fc62f82482f0
Daniel Büchele <daniel@buechele.cc>
2017-02-20 21:34:21 +0100

    eventbutton

--- site/tags/eventbutton.php
+++ site/tags/eventbutton.php
@@ -1,10 +1,7 @@
 <?php
 	kirbytext::$tags['eventbutton'] = array(
 		'html' => function($tag) {
-			$r = '<a class="btn btn-danger btn-block" target="_blank" onclick="fbq(\'track\', \'CompleteRegistration\');" href="'.$tag->attr('link').'">';
-			$r .= $tag->attr('eventbutton');
-			$r .= '</a>';
-			return $r;
+			return '<a class="btn btn-danger btn-block" target="_blank" onclick="fbq(\'track\', \'CompleteRegistration\');" href="' . $tag->attr('eventbutton') . '">' . $tag->attr('text') . '</a>';
 		}
 	);
 ?>
