289b6ad6daa8629ffeca2f84f405e97fc9a1390c
Daniel Büchele <daniel@buechele.cc>
2019-09-01 23:13:50 +0100

    fix date display

--- site/snippets/date.php
+++ site/snippets/date.php
@@ -1,7 +1,8 @@
+<?php setlocale(LC_ALL, 'de_DE'); ?>
 <div class="date">
   <div class="day">
     <?=$date->toDate('%d') ?>
   </div>
-  <?=$date->toDate('%M') ?>
+  <?=$date->toDate('%b') ?>
   <?=$date->toDate('%y') ?>
 </div>
