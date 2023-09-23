e959c6a5c82a99d01a67ea123aeade4b5845ffed
Veronika Fuchsberger <veronika.fuchsberger@thetrainline.com>
2023-09-23 17:16:12 +0100

    add additives to speisekarte

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,3 +1,4 @@
+import { InfoIcon } from '@chakra-ui/icons';
 import {
   AccordionButton,
   AccordionIcon,
@@ -9,14 +10,29 @@ import {
   Heading,
   ListItem,
   Text,
+  Tooltip,
   UnorderedList,
+  useBreakpointValue,
 } from '@chakra-ui/react';
-import type { ProductList } from '~/types/graphql';
+import type { FC } from 'react';
+import type { ProductAdditives, ProductList } from '~/types/graphql';
+
+
+const TooltipContent: FC<{additives: ProductAdditives[]}> = ({additives}) => {
+ return (
+  <UnorderedList listStyleType={'none'} marginInlineStart={0} padding={1}>
+    {additives.map((additive: ProductAdditives) => (<Text key={additive.id} fontWeight={'normal'}>{additive.displayName}</Text>))}
+  </UnorderedList>
+ ) 
+}
 
 export default function ProductList({ productList }: { productList: ProductList }) {
   const showDepositText = productList.product.some(
     (item) => item.requiresDeposit,
   );
+  const productListLength = productList.product.length;
+  const tooltipPlacement = useBreakpointValue({base: 'top', lg: 'right'});
+
   return (
     <AccordionItem>
       <Heading>
@@ -28,26 +44,31 @@ export default function ProductList({ productList }: { productList: ProductList 
         </AccordionButton>
       </Heading>
       <AccordionPanel pb={4}>
-        <UnorderedList listStyleType={'none'}>
-          {productList.product.map((item) => (
+        <UnorderedList listStyleType={'none'} marginInlineStart={0}>
+          {productList.product.map((item, index) => (
             <ListItem key={item.name}>
-              <HStack justifyContent={'space-between'}>
-                <span>{item.name}</span>
+              <HStack justifyContent={'space-between'} paddingY={1}>
+                <Text>
+                  {item.name}
+                  {item.additives.length > 0 && <Tooltip label={<TooltipContent additives={item.additives} />} hasArrow placement={tooltipPlacement}>
+                    <InfoIcon color={'offwhite.300'} marginLeft={1} />
+                  </Tooltip>}
+                </Text>
                 <span>
-                  {item.requiresDeposit && <span>* </span>}
+                  {item.requiresDeposit && <Text color={'offwhite.500'} as={'span'}>* </Text>}
                   {new Intl.NumberFormat('de-DE', {
                     style: 'currency',
                     currency: 'EUR',
                   }).format(item.price / 100)}
                 </span>
               </HStack>
+              {(showDepositText || index < productListLength - 1) && <Divider />}
             </ListItem>
           ))}
         </UnorderedList>
         {showDepositText && (
           <>
-            <Divider />
-            <Text>
+            <Text textAlign={'right'} paddingTop={1} color={'offwhite.500'}>
               * zuzüglich{' '}
               {new Intl.NumberFormat('de-DE', {
                 style: 'currency',
