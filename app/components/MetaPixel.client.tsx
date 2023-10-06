bce9167bfb8db84acdf3562ef733d2b8eec03346
Daniel Büchele <daniel@buechele.cc>
2023-10-06 01:03:37 +0100

    pxl

--- app/components/MetaPixel.client.tsx
+++ app/components/MetaPixel.client.tsx
@@ -3,7 +3,7 @@ import ReactPixel from 'react-facebook-pixel';
 
 export default function MetaPixel() {
   useEffect(() => {
-    ReactPixel.init('807706852681161');
+    ReactPixel.init('568483009893821');
     ReactPixel.pageView();
   }, []);
   return (
@@ -14,7 +14,7 @@ export default function MetaPixel() {
           height="1"
           width="1"
           style={{display: 'none'}}
-          src="https://www.facebook.com/tr?id=807706852681161&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=568483009893821&ev=PageView&noscript=1"
         />
       </noscript>
     </>
