5700a3420c98928c8916cd265b5f87085f8c3c95
Daniel Büchele <daniel@buechele.cc>
2024-01-20 22:06:50 +0000

    nuclino SSO

--- app/routes/nuclino-sso.tsx
+++ app/routes/nuclino-sso.tsx
@@ -9,13 +9,14 @@ import {
   Spinner,
   Divider,
 } from '@chakra-ui/react';
-import {Form, useNavigate} from '@remix-run/react';
 import {useRef, useState} from 'react';
 import {
   CheckNonceRequestDocument,
   useCreateNonceRequestMutation,
 } from '~/types/graphql';
 import mergeMeta from '~/utils/mergeMeta';
+import {FaSlack} from 'react-icons/fa6';
+import {useSearchParams} from '@remix-run/react';
 
 gql`
   mutation CreateNonceRequest($email: String!) {
@@ -40,6 +41,7 @@ export default function Sso() {
   const apolloClient = useApolloClient();
   const [email, setEmail] = useState('');
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [searchParams] = useSearchParams();
 
   return (
     <VStack spacing="10">
@@ -55,73 +57,91 @@ export default function Sso() {
           </Box>
         ) : (
           <>
-            <VStack spacing="1">
+            <VStack
+              as="form"
+              w="100%"
+              spacing="2"
+              onSubmit={(e) => {
+                e.preventDefault();
+                requestNonce({
+                  variables: {
+                    email,
+                  },
+                }).then(({data}) => {
+                  if (timeoutRef.current) {
+                    clearTimeout(timeoutRef.current);
+                  }
+                  if (!data?.createNonceRequest) {
+                    return;
+                  }
+
+                  let counter = 0;
+
+                  const checkNonceRequest = async () => {
+                    counter++;
+                    if (counter > 600) {
+                      return;
+                    }
+                    const {data: d} = await apolloClient.mutate({
+                      mutation: CheckNonceRequestDocument,
+                      variables: {
+                        nonceRequestId: data.createNonceRequest,
+                      },
+                    });
+                    if (d?.nonceFromRequest) {
+                      const url = new URL(
+                        'https://api.kulturspektakel.de/saml/login',
+                      );
+                      searchParams.forEach((value, key) =>
+                        url.searchParams.set(key, value),
+                      );
+                      url.searchParams.set('nonce', d.nonceFromRequest);
+                      window.location.href = url.toString();
+                    } else {
+                      timeoutRef.current = setTimeout(checkNonceRequest, 500);
+                    }
+                  };
+
+                  timeoutRef.current = setTimeout(checkNonceRequest, 500);
+                });
+              }}
+            >
               <Heading as="h2" size="md">
-                Slack
+                <FaSlack style={{display: 'inline-block', marginBottom: -2}} />
+                &nbsp;Slack
               </Heading>
               <Text>
                 Gib deine E-Mailaddresse ein, mit der du bei Slack registriert
                 bist und bestätige deinen Login in der Slack-App.
               </Text>
-              <Form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  requestNonce({
-                    variables: {
-                      email,
-                    },
-                  }).then(({data}) => {
-                    if (timeoutRef.current) {
-                      clearTimeout(timeoutRef.current);
-                    }
-                    if (!data?.createNonceRequest) {
-                      return;
-                    }
 
-                    const checkNonceRequest = async () => {
-                      const {data: d} = await apolloClient.mutate({
-                        mutation: CheckNonceRequestDocument,
-                        variables: {
-                          nonceRequestId: data.createNonceRequest,
-                        },
-                      });
-                      if (d?.nonceFromRequest) {
-                        const url = new URL(
-                          'https://api.kulturspektakel.de/slack/token',
-                        );
-                        url.searchParams.set('nonce', d.nonceFromRequest);
-                        window.location.href = url.toString();
-                      } else {
-                        timeoutRef.current = setTimeout(checkNonceRequest, 500);
-                      }
-                    };
-
-                    timeoutRef.current = setTimeout(checkNonceRequest, 500);
-                    return () => clearTimeout(timeoutRef.current!);
-                  });
-                }}
+              <Input
+                placeholder="Slack E-Mailadresse"
+                type="email"
+                value={email}
+                bg="white"
+                w="100%"
+                onChange={(e) => setEmail(e.target.value.trim())}
+              />
+              <Button
+                w="100%"
+                type="submit"
+                isLoading={loading}
+                variant="primary"
               >
-                <Input
-                  placeholder="Slack E-Mailadresse"
-                  type="email"
-                  value={email}
-                  bg="white"
-                  onChange={(e) => setEmail(e.target.value.trim())}
-                />
-                <Button
-                  w="100%"
-                  type="submit"
-                  isLoading={loading}
-                  variant="primary"
-                >
-                  Einloggen
-                </Button>
-              </Form>
+                Einloggen
+              </Button>
             </VStack>
 
             <Divider mt="10" mb="10" />
 
-            <VStack spacing="1">
+            <VStack
+              as="form"
+              w="100%"
+              spacing="2"
+              method="post"
+              action={`https://api.kulturspektakel.de/saml/login?${searchParams.toString()}`}
+            >
               <Heading as="h2" size="md">
                 Passwort
               </Heading>
@@ -129,17 +149,16 @@ export default function Sso() {
                 Falls du keinen Slack-Account hast, frage jemanden aus der Crew
                 nach dem Passwort für das Wiki.
               </Text>
-              <Form>
-                <Input
-                  display="block"
-                  placeholder="Passwort"
-                  type="password"
-                  bg="white"
-                />
-                <Button w="100%" type="submit" variant="primary">
-                  Einloggen
-                </Button>
-              </Form>
+              <Input
+                display="block"
+                placeholder="Passwort"
+                type="password"
+                bg="white"
+                name="password"
+              />
+              <Button w="100%" type="submit" variant="primary">
+                Einloggen
+              </Button>
             </VStack>
           </>
         )}
