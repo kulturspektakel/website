6fe556b509a2716e9b9fbf320c60aefe9e285770
Daniel Büchele <daniel@buechele.cc>
2025-11-01 10:20:15 +0000

    remove spendenbox from landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
-import {Box, Center, Separator, Button} from '@chakra-ui/react';
+import {Center, Separator} from '@chakra-ui/react';
 import React from 'react';
 import Article from '../components/news/Article';
 import LinkButton from '../components/LinkButton';
-import {createFileRoute, Link} from '@tanstack/react-router';
+import {createFileRoute} from '@tanstack/react-router';
 import {createServerFn} from '@tanstack/react-start';
 import {prismaClient} from '../utils/prismaClient';
 import {markdownPages} from '../utils/markdownText';
-import {SpendenBox} from '../components/spenden/box';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -14,58 +13,29 @@ export const Route = createFileRoute('/')({
 });
 
 const newsLoader = createServerFn().handler(async () => {
-  const [sum, count, news] = await Promise.all([
-    prismaClient.donation.aggregate({
-      _sum: {
-        amount: true,
-      },
-    }),
-    prismaClient.donation.aggregate({
-      _count: {
-        id: true,
-      },
-      where: {
-        source: {
-          not: 'Other',
-        },
-      },
-    }),
-    prismaClient.news.findMany({
-      take: 10,
-      orderBy: {
-        createdAt: 'desc',
-      },
-      select: {
-        title: true,
-        slug: true,
-        createdAt: true,
-        content: true,
-      },
-    }),
-  ]);
+  const news = await prismaClient.news.findMany({
+    take: 10,
+    orderBy: {
+      createdAt: 'desc',
+    },
+    select: {
+      title: true,
+      slug: true,
+      createdAt: true,
+      content: true,
+    },
+  });
 
   return {
     news: await markdownPages(news),
-    total: sum._sum.amount ?? 0,
-    count: count._count.id ?? 0,
   };
 });
 
 export function Index() {
-  const {news, total, count} = Route.useLoaderData();
+  const {news} = Route.useLoaderData();
 
   return (
     <>
-      <Box mb="12" mt="-32">
-        <SpendenBox
-          donors={count}
-          total={total}
-          secondaryLabel="Mehr erfahren"
-          secondaryLinkProps={{
-            to: '/spenden',
-          }}
-        />
-      </Box>
       {news.map((item, i) => (
         <React.Fragment key={i}>
           {i > 0 && <Separator width="60%" m="auto" mb="16" />}
