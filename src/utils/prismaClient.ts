b5095856a379598fe6b1824a39197fb8cc0d6e4b
Daniel Büchele <daniel@buechele.cc>
2025-11-26 00:00:01 +0000

    neon

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,3 +1,17 @@
 import {PrismaClient} from '@prisma/client';
+import {PrismaNeon} from '@prisma/adapter-neon';
+import {neonConfig} from '@neondatabase/serverless';
+import ws from 'ws';
 
-export const prismaClient = new PrismaClient();
+neonConfig.webSocketConstructor = ws;
+
+// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true;
+
+export const prismaClient =
+  global.prisma ||
+  new PrismaClient({
+    adapter: new PrismaNeon({
+      connectionString: process.env.DATABASE_URL + '&connect_timeout=10',
+    }),
+  });
