68120d4edd5a44687c5ec20c3daa2ac038e52d55
Daniel Büchele <daniel@buechele.cc>
2026-02-07 23:04:33 +0000

    check in generated files

--- src/server/routes/__root.ts
+++ src/server/routes/__root.ts
@@ -1,9 +1,6 @@
 import {createServerFn} from '@tanstack/react-start';
 import {getCurrentEvent} from '../../utils/getCurrentEvent.server';
-// import {staticFunctionMiddleware} from '@tanstack/start-static-server-functions';
 
-export const beforeLoad = createServerFn()
-  // .middleware([staticFunctionMiddleware])
-  .handler(async () => ({
-    event: await getCurrentEvent(),
-  }));
+export const beforeLoad = createServerFn().handler(async () => ({
+  event: await getCurrentEvent(),
+}));
